f397a025c58d2fe686c0d98a9a7f4d2d44eb52de
Miroslav Kovac <miro1.kovac@gmail.com>
2018-02-13 12:54:58 +0100

    tests

--- playground/test-component.spec.ts
+++ playground/test-component.spec.ts
@@ -1,8 +1,51 @@
-console.log('running test 1');
-describe('Something abstract', function () {
-  describe('Something specific', function () {
-    it('Expects truth to be true', function () {
-      expect(true).toBe(true, 'Expectation failed');
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { TestComponent } from './test-component';
+import { SelectableDirective, SelectableModule, SelectFrameComponent } from '../src/index';
+import { ComponentFixtureAutoDetect } from '@angular/core/testing';
+
+describe('TestComponent', () => {
+
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
+  let h1Element: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent, SelectFrameComponent, SelectableDirective],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true }
+      ]
     });
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    htmlElement = debugElement.query(By.directive(SelectableDirective)).nativeElement;
+    h1Element = debugElement.query(By.css('h1')).nativeElement;
+  });
+  it('component has title', () => {
+    // fixture.detectChanges();
+    expect(h1Element.title).toEqual('Testing the component!');
+  });
+  it('component has directive', () => {
+    // fixture.detectChanges();
+    expect(htmlElement).toBeDefined('no selectable directives found!');
+  });
+
+
+});
+
+
+
+
+
+
+describe('Something abstract', () => {
+  describe('Something specific', () => {
+    it('Expects truth to be true', () => expect(true).toBe(true, 'Expectation failed'));
   });
 });
--- playground/test-component.ts
+++ playground/test-component.ts
@@ -2,11 +2,25 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'test-component',
-    templateUrl: 'name.component.html'
+    template: `
+    <h1>{{componentTitle}}</h1>
+    <div class="frame full-height" app-selectFrame
+    [ensureSame]="['ShippingType','GLN']" [filter]="{ allowSelectElements: true, thisElement: true }" (data)="onData($event)">
+    <div selectable
+    [select]="{'Id':1,'Name':'firstName','type':'firstType'}" [selectScope]="firstScope">
+    </div>
+    </div>
+    `
 })
 
 export class TestComponent implements OnInit {
+    firstScope = 'firstScope';
+    componentTitle = 'Testing the component!';
     constructor() { }
 
     ngOnInit() { }
+
+    onData(ev: any) {
+        return ev;
+    }
 }
